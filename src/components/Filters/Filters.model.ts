e2f1a9e68f1fce64003c2e8ac930fec765b4b873
julian.romero <julianromero916@hotmail.com>
2022-09-22 00:57:25 -0300

    feat: add filter buttton for text search, add modal component with test

---
+++ src/components/Filters/Filters.model.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin: 30px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.h2`
+  font-weight: bold;
+`;
+
+export const ButtonWrapper = styled.button`
+  background: white;
+  border: none;
+  box-shadow: 1px 1px #d4d4d4;
+  border-radius: 30%;
+  padding: 1px 4px;
+
+  :active {
+    transform: scale(0.98);
+    box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+  }
+  
+  img {
+    opacity: 0.5;
+    vertical-align: middle;
+  }
+`;
